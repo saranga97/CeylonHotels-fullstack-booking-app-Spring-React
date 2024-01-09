0527a8f58ff4e4d034599d2e3212fd9d88e02860
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-01-09 17:36:13 +0530

    Starter Files

---
+++ src/main/java/com/wavelabs/ceylonhotels/CeylonHotelsApplication.java
@@ -0,0 +1,13 @@
+package com.wavelabs.ceylonhotels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeylonHotelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeylonHotelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wavelabs/ceylonhotels/CeylonHotelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wavelabs.ceylonhotels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeylonHotelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
