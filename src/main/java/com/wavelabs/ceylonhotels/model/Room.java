de14027fdeccd0970850a9bfe57a64509a1e0096
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-01-10 18:34:54 +0530

    Room Class updates

--- src/main/java/com/wavelabs/ceylonhotels/model/Room.java
+++ src/main/java/com/wavelabs/ceylonhotels/model/Room.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,12 +19,18 @@ import java.util.List;
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
+
     private String roomType;
+
     private BigDecimal roomPrice;
+
     private boolean isBooked = false;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @Lob
+    private Blob photo;
+
+    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<BookedRoom> bookings;
 
     public Room() {
@@ -35,5 +43,9 @@ public class Room {
         }
         bookings.add(booking);
         booking.setRoom(this);
+        isBooked = true;
+
+        String BookingCode = RandomStringUtils.random(10);
+        booking.setBookingConfirmationCode(BookingCode);
     }
 }
