a05cea11a3d47da03e09f40eb754f5bc89d83eeb
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-01-10 18:34:26 +0530

    Front End Response Controller classes added

---
+++ src/main/java/com/wavelabs/ceylonhotels/response/BookingResponse.java
@@ -0,0 +1,42 @@
+package com.wavelabs.ceylonhotels.response;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingResponse {
+    private Long bookingId;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
+
+    private String guestFullName;
+
+    private String guestEmail;
+
+    private int NumOfAdults;
+
+    private int NumOfChildren;
+
+    private int totalNumOfGuest;
+
+    private String bookingConfirmationCode;
+
+    private RoomResponse room;
+
+    public BookingResponse(Long bookingId,
+                           LocalDate checkInDate, LocalDate checkOutDate,
+                           String bookingConfirmationCode) {
+        this.bookingId = bookingId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+}
