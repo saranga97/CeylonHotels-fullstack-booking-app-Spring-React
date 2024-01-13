f21c4df28c667d79df5762f980fc377a44d6b03c
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-01-14 00:48:05 +0530

    Room Controller impl

--- src/main/java/com/wavelabs/ceylonhotels/controller/RoomController.java
+++ src/main/java/com/wavelabs/ceylonhotels/controller/RoomController.java
@@ -1,4 +1,36 @@
 package com.wavelabs.ceylonhotels.controller;
 
+import com.wavelabs.ceylonhotels.model.Room;
+import com.wavelabs.ceylonhotels.response.RoomResponse;
+import com.wavelabs.ceylonhotels.service.IRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/rooms")
 public class RoomController {
+    private final IRoomService roomService;
+    @PostMapping("/add/new-room")
+    public ResponseEntity<RoomResponse> addNewRoom (
+            @RequestParam("photo") MultipartFile photo,
+            @RequestParam("roomType") String roomType,
+            @RequestParam("roomPrice") BigDecimal roomPrice
+            ) throws SQLException, IOException {
+        Room savedRoom = roomService.addNewRoom(photo,roomType,roomPrice);
+        RoomResponse response = new RoomResponse(
+                savedRoom.getId(),
+                savedRoom.getRoomType(),
+                savedRoom.getRoomPrice()
+        );
+        return ResponseEntity.ok(response);
+    }
 }
