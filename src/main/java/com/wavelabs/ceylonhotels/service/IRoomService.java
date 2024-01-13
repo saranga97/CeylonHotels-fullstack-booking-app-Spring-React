dcc152d90c120e92e02d68606108d88691dcb325
Saranga Nayanajith <karunasundara_kmsn_e21@engug.ruh.ac.lk>
2024-01-14 00:47:49 +0530

    Room Service impl

--- src/main/java/com/wavelabs/ceylonhotels/service/IRoomService.java
+++ src/main/java/com/wavelabs/ceylonhotels/service/IRoomService.java
@@ -1,4 +1,12 @@
 package com.wavelabs.ceylonhotels.service;
 
+import com.wavelabs.ceylonhotels.model.Room;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
 public interface IRoomService {
+    Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice) throws IOException, SQLException;
 }
--- src/main/java/com/wavelabs/ceylonhotels/service/RoomServiceImp.java
+++ src/main/java/com/wavelabs/ceylonhotels/service/RoomServiceImp.java
@@ -1,4 +1,32 @@
 package com.wavelabs.ceylonhotels.service;
 
-public class RoomServiceImp {
+import com.wavelabs.ceylonhotels.model.Room;
+import com.wavelabs.ceylonhotels.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+@Service
+@RequiredArgsConstructor
+public class RoomServiceImp implements IRoomService{
+    private final RoomRepository roomRepository;
+    @Override
+    public Room addNewRoom(MultipartFile file, String roomType, BigDecimal roomPrice) throws SQLException, IOException {
+        Room room = new Room();
+        room.setRoomType(roomType);
+        room.setRoomPrice(roomPrice);
+        if(!file.isEmpty()){
+            byte[] photoBytes = file.getBytes();
+            Blob photoBlob = new SerialBlob(photoBytes);
+            room.setPhoto(photoBlob);
+        }
+
+        return roomRepository.save(room);
+    }
 }
